32a02ebebeb94aeeda976fb1fc86565cecab0c06
ArbuzovDmtr <arbuzov.dmtr@gmail.com>
2026-05-06 13:09:29 +0200

    begin work with OAuth

---
+++ backend/src/main/java/org/example/backend/User/Role.java
@@ -0,0 +1,6 @@
+package org.example.backend.User;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- backend/src/main/java/org/example/backend/User/User.java
+++ backend/src/main/java/org/example/backend/User/User.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.Instant;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -15,10 +16,13 @@ public class User {
 
     @Id
     private String id;
-
-    private String username;
     private String email;
-    private String passwordHash;
+    private String name;
+    private String avatarUrl;
+    private String provider;
+    private String providerId;
+    @Builder.Default
+    private Set<Role> roles = Set.of(Role.USER);
     @Builder.Default
     private Instant createdAt = Instant.now();
 }
